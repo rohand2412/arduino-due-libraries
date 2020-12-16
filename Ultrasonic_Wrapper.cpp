67556781cbfbaefc7ed2004b328ae2a1545b21e9
Rohan Dugad <rohand2468@gmail.com>
2020-12-15 21:09:20 -0500

    Replaced placement new with array of pointers

--- Ultrasonic_Wrapper.cpp
+++ Ultrasonic_Wrapper.cpp
@@ -9,13 +9,11 @@ Ultrasonic_Wrapper::Ultrasonic_Wrapper(unsigned int trigPin,
                                        _trigPin(trigPin),
                                        _sensorNum(sensorNum)
 {
-    char *ultrasonics = new char[_sensorNum * sizeof(Ultrasonic)];
+    _ultrasonics = new Ultrasonic *[_sensorNum];
     for (unsigned int i = 0; i < _sensorNum; i++)
     {
-        Ultrasonic *ultrasonic = new (&ultrasonics[i * sizeof(Ultrasonic)])
-        Ultrasonic(_trigPin, *echoPins++, *burstFrequencies++); //end of previous line
+        _ultrasonics[i] = new Ultrasonic(_trigPin, *echoPins++, *burstFrequencies++);
     }
-    _ultrasonics = reinterpret_cast<Ultrasonic *>(ultrasonics);
 }
 
 Ultrasonic_Wrapper::Ultrasonic_Wrapper(unsigned int trigPin,
@@ -25,13 +23,11 @@ Ultrasonic_Wrapper::Ultrasonic_Wrapper(unsigned int trigPin,
                                        _trigPin(trigPin),
                                        _sensorNum(sensorNum)
 {
-    char *ultrasonics = new char[_sensorNum * sizeof(Ultrasonic)];
+    _ultrasonics = new Ultrasonic *[_sensorNum];
     for (unsigned int i = 0; i < _sensorNum; i++)
     {
-        Ultrasonic *ultrasonic = new (&ultrasonics[i * sizeof(Ultrasonic)])
-        Ultrasonic(_trigPin, *echoPins++, burstFrequency); //end of previous line
+        _ultrasonics[i] = new Ultrasonic(_trigPin, *echoPins++, burstFrequency);
     }
-    _ultrasonics = reinterpret_cast<Ultrasonic *>(ultrasonics);
 }
 
 Ultrasonic_Wrapper::Ultrasonic_Wrapper(unsigned int trigPin,
@@ -40,20 +36,18 @@ Ultrasonic_Wrapper::Ultrasonic_Wrapper(unsigned int trigPin,
                                        _trigPin(trigPin),
                                        _sensorNum(1)
 {
-    char *ultrasonics = new char[_sensorNum * sizeof(Ultrasonic)];
+    _ultrasonics = new Ultrasonic *[_sensorNum];
     for (unsigned int i = 0; i < _sensorNum; i++)
     {
-        Ultrasonic *ultrasonic = new (&ultrasonics[i * sizeof(Ultrasonic)])
-        Ultrasonic(_trigPin, echoPin, burstFrequency); //end of previous line
+        _ultrasonics[i] = new Ultrasonic(_trigPin, echoPin, burstFrequency);
     }
-    _ultrasonics = reinterpret_cast<Ultrasonic *>(ultrasonics);
 }
 
 void Ultrasonic_Wrapper::begin(void (*externalEchoPinISRs[])())
 {
     for (unsigned int i = 0; i < _sensorNum; i++)
     {
-        (*(_ultrasonics + i)).begin(externalEchoPinISRs[i]);
+        (*(*_ultrasonics + i)).begin(externalEchoPinISRs[i]);
     }
 }
 
@@ -61,7 +55,7 @@ void Ultrasonic_Wrapper::begin(void (*externalEchoPinISR)())
 {
     for (unsigned int i = 0; i < _sensorNum; i++)
     {
-        (*(_ultrasonics + i)).begin(externalEchoPinISR);
+        (*(*_ultrasonics + i)).begin(externalEchoPinISR);
     }
 }
 
@@ -69,18 +63,18 @@ void Ultrasonic_Wrapper::update()
 {
     for (unsigned int i = 0; i < _sensorNum; i++)
     {
-        (*(_ultrasonics + i)).update();
+        (*(*_ultrasonics + i)).update();
     }
 }
 
 unsigned int Ultrasonic_Wrapper::getDistance(unsigned int index /*= 0*/) const
 {
-    return (*(_ultrasonics + index)).getDistance();
+    return (*(*_ultrasonics + index)).getDistance();
 }
 
 unsigned int Ultrasonic_Wrapper::getEchoPin(unsigned int index /*= 0*/) const
 {
-    return (*(_ultrasonics + index)).getEchoPin();
+    return (*(*_ultrasonics + index)).getEchoPin();
 }
 
 unsigned int Ultrasonic_Wrapper::getTrigPin() const
@@ -90,5 +84,5 @@ unsigned int Ultrasonic_Wrapper::getTrigPin() const
 
 void Ultrasonic_Wrapper::echoPinISR(unsigned int index /*= 0*/)
 {
-    (*(_ultrasonics + index)).echoPinISR();
+    (*(*_ultrasonics + index)).echoPinISR();
 }
