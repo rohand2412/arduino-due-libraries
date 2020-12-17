13eaf5c2b677bf420fdb3bf5045625640c991aaf
Rohan Dugad <rohand2468@gmail.com>
2020-12-17 01:13:15 -0500

    Created read() method

--- Servo_Wrapper.cpp
+++ Servo_Wrapper.cpp
@@ -5,13 +5,32 @@ Servo_Wrapper::Servo_Wrapper(unsigned int lowerBound /*= 0*/,
                              Servo(), 
                              _LOWER_BOUND(lowerBound), 
                              _UPPER_BOUND(upperBound)
-{}; 
+{
+    _value = 0xFFFFFFFF;    //_value = -1
+};
 
 void Servo_Wrapper::write(unsigned int value)
 {
+    _value = value;
     value += _LOWER_BOUND;                                  //Translate angle to bound range
     value = (value > _UPPER_BOUND) ? _UPPER_BOUND : value;  //Cap value at _upperBound
-    Servo::write(value);                                    //Pass value to Servo::write()
+    Servo::write(value); //Pass value to Servo::write()
+}
+
+unsigned int Servo_Wrapper::read() const
+{
+    //Need to pass RGB LED object to class when RGB class completed
+    //Flash Red if read is called before _value is initialized
+    //Until then Serial Port will be only form of indication
+    if (_value == 0xFFFFFFFF)   //_value == -1
+    {
+        while(true)
+        {
+            Serial.print("Servo_Wrapper::read() called before ");
+            Serial.println("initialization of Servo_Wrapper::write()");
+        }
+    }
+    return _value;
 }
 
 unsigned int Servo_Wrapper::getLowerBound() const
--- Servo_Wrapper.h
+++ Servo_Wrapper.h
@@ -7,13 +7,17 @@ class Servo_Wrapper : public Servo
     private:
         const unsigned int _LOWER_BOUND;
         const unsigned int _UPPER_BOUND;
-    
+
+        unsigned int _value;
+
     public:
         Servo_Wrapper(unsigned int lowerBound = 0, unsigned int upperBound = 180);
 
         void write(unsigned int value);
 
         void writeMicroseconds(int value) = delete; //Only method to set degree should be write()
+
+        unsigned int read() const;
 
         int readMicroseconds() = delete;    //Unnecessary without writeMicroseconds()
 
