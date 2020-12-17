57859358e4b105b9a19ca0ea324090e545ea3b21
Rohan Dugad <rohand2468@gmail.com>
2020-12-16 22:56:20 -0500

    Replaced const_cast with optional constructor parameters

--- Servo_Wrapper.cpp
+++ Servo_Wrapper.cpp
@@ -1,20 +1,17 @@
 #include "Servo_Wrapper.h"
 
-Servo_Wrapper::Servo_Wrapper() : Servo(), _LOWER_BOUND(0), _UPPER_BOUND(180){}; 
+Servo_Wrapper::Servo_Wrapper(unsigned int lowerBound /*= 0*/,
+                             unsigned int upperBound /*= 180*/) : 
+                             Servo(), 
+                             _LOWER_BOUND(lowerBound), 
+                             _UPPER_BOUND(upperBound)
+{}; 
 
 void Servo_Wrapper::write(unsigned int value)
 {
     value += _LOWER_BOUND;                                  //Translate angle to bound range
     value = (value > _UPPER_BOUND) ? _UPPER_BOUND : value;  //Cap value at _upperBound
     Servo::write(value);                                    //Pass value to Servo::write()
-}
-
-void Servo_Wrapper::setBounds(unsigned int lowerBound, unsigned int upperBound)
-{
-    unsigned int& lb = const_cast <unsigned int &> (_LOWER_BOUND);
-    lb = lowerBound;
-    unsigned int &ub = const_cast <unsigned int &> (_UPPER_BOUND);
-    ub = upperBound;
 }
 
 unsigned int Servo_Wrapper::getLowerBound() const
--- Servo_Wrapper.h
+++ Servo_Wrapper.h
@@ -5,19 +5,17 @@
 class Servo_Wrapper : public Servo
 {
     private:
-        const volatile unsigned int _LOWER_BOUND;
-        const volatile unsigned int _UPPER_BOUND;
+        const unsigned int _LOWER_BOUND;
+        const unsigned int _UPPER_BOUND;
     
     public:
-        Servo_Wrapper();
+        Servo_Wrapper(unsigned int lowerBound = 0, unsigned int upperBound = 180);
 
         void write(unsigned int value);
 
         void writeMicroseconds(int value) = delete; //Only method to set degree should be write()
 
         int readMicroseconds() = delete;    //Unnecessary without writeMicroseconds()
-
-        void setBounds(unsigned int lowerBound, unsigned int upperBound);
 
         unsigned int getLowerBound() const;
 
