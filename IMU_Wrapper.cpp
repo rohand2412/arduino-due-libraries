b72de8491b29b8fbfd7b6e8bac24b9e1b6c7af0e
Rohan Dugad <rohand2468@gmail.com>
2021-01-16 22:37:33 -0500

    Made IMU_Wrapper comply with Adafruit libs

--- IMU_Wrapper.cpp
+++ IMU_Wrapper.cpp
@@ -1,52 +1,150 @@
+#include <Arduino.h>
 #include "IMU_Wrapper.h"
 
-IMU_Wrapper::IMU_Wrapper(unsigned int mpuInterruptPin) : IMU(mpuInterruptPin)
+IMU_Wrapper::IMU_Wrapper(unsigned int sensorID /*= 55*/, uint8_t address /*= 0x28*/)
 {
-    _oldYawRaw = 0;
-    _oldPitchRaw = 0;
-    _oldRollRaw = 0;
-    _yaw = 0;
-    _pitch = 0;
-    _roll = 0;
+  _bno = new Adafruit_BNO055(sensorID, address);
+}
+
+void IMU_Wrapper::begin(Adafruit_BNO055::adafruit_bno055_opmode_t mode /*= Adafruit_BNO055::adafruit_bno055_opmode_t::OPERATION_MODE_NDOF*/)
+{
+  if(!_bno->begin(mode))
+  {
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+
+  delay(1000);
+
+  _bno->setExtCrystalUse(true);
+
+  update();
 }
 
 void IMU_Wrapper::update()
 {
-  IMU::update();
-  float yawRaw = IMU::getYaw();
-  float pitchRaw = IMU::getPitch();
-  float rollRaw = IMU::getRoll();
+  delay(_BNO055_SAMPLERATE_DELAY_MS);
+
+  _bno->getSensor(&_sensor);
+
+  _system_status = 0;
+  _self_test_results = 0;
+  _system_error = 0;
+  _bno->getSystemStatus(&_system_status, &_self_test_results, &_system_error);
+
+  _systemCal = 0;
+  _gyroCal = 0;
+  _accelCal = 0;
+  _magCal = 0;
+  _bno->getCalibration(&_systemCal, &_gyroCal, &_accelCal, &_magCal);
+
+  _bno->getEvent(&_event);
+  double yawRaw = _event.orientation.x;
+  double pitchRaw = _event.orientation.y;
+  double rollRaw = _event.orientation.z;
   _overflow(_oldYawRaw, yawRaw, _yaw);
   _overflow(_oldPitchRaw, pitchRaw, _pitch);
   _overflow(_oldRollRaw, rollRaw, _roll);
 }
 
-void IMU_Wrapper::reset(float yaw /*=0*/, float pitch /*=0*/, float roll /*=0*/)
+void IMU_Wrapper::reset(double yaw /*=0*/, double pitch /*=0*/, double roll /*=0*/)
 {
   _yaw = yaw;
   _pitch = pitch;
   _roll = roll;
 }
 
-float IMU_Wrapper::getYaw() const
+/**************************************************************************/
+/*
+    Displays some basic information on this sensor from the unified
+    sensor API sensor_t type (see Adafruit_Sensor for more information)
+*/
+/**************************************************************************/
+void IMU_Wrapper::displaySensorDetails()
+{
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(_sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(_sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(_sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(_sensor.max_value); Serial.println(" xxx");
+  Serial.print  ("Min Value:    "); Serial.print(_sensor.min_value); Serial.println(" xxx");
+  Serial.print  ("Resolution:   "); Serial.print(_sensor.resolution); Serial.println(" xxx");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
+/**************************************************************************/
+/*
+    Display some basic info about the sensor status
+*/
+/**************************************************************************/
+void IMU_Wrapper::displaySensorStatus()
+{
+  Serial.println("");
+  Serial.print("System Status: 0x");
+  Serial.println(_system_status, HEX);
+  Serial.print("Self Test:     0x");
+  Serial.println(_self_test_results, HEX);
+  Serial.print("System Error:  0x");
+  Serial.println(_system_error, HEX);
+  Serial.println("");
+  delay(500);
+}
+
+/**************************************************************************/
+/*
+    Display sensor calibration status
+*/
+/**************************************************************************/
+void IMU_Wrapper::displayCalStatus()
+{
+  Serial.print("\t");
+  if (!_systemCal)
+  {
+    Serial.print("! ");
+  }
+
+  Serial.print("Sys:");
+  Serial.print(_systemCal, DEC);
+  Serial.print(" G:");
+  Serial.print(_gyroCal, DEC);
+  Serial.print(" A:");
+  Serial.print(_accelCal, DEC);
+  Serial.print(" M:");
+  Serial.print(_magCal, DEC);
+  Serial.print("\n");
+}
+
+void IMU_Wrapper::displayOrientation()
+{
+  Serial.print("Yaw: ");
+  Serial.print(getYaw(), 4);
+  Serial.print("\tPitch: ");
+  Serial.print(getPitch(), 4);
+  Serial.print("\tRoll: ");
+  Serial.print(getRoll(), 4);
+}
+
+double IMU_Wrapper::getYaw() const
 {
   return _yaw;
 }
 
-float IMU_Wrapper::getPitch() const
+double IMU_Wrapper::getPitch() const
 {
   return _pitch;
 }
 
-float IMU_Wrapper::getRoll() const
+double IMU_Wrapper::getRoll() const
 {
   return _roll;
 }
 
-void IMU_Wrapper::_overflow(float& oldRaw, float& raw, float& axis)
+void IMU_Wrapper::_overflow(double& oldRaw, double& raw, double& axis)
 {
-  float threshold = 300; //Num of deg at which event is considered overflow
-  float fullCircleDeg = 360; //Num of deg in a circle
+  double threshold = 300; //Num of deg at which event is considered overflow
+  double fullCircleDeg = 360; //Num of deg in a circle
   if (raw - oldRaw > threshold)
   {
     axis -= fullCircleDeg;
--- IMU_Wrapper.h
+++ IMU_Wrapper.h
@@ -1,30 +1,55 @@
 #pragma once
 #include <Arduino.h>
-#include "IMU.h"
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <imumaths.h>
 
-class IMU_Wrapper : public IMU
+class IMU_Wrapper
 {
     private:
-        float _oldYawRaw;
-        float _oldPitchRaw;
-        float _oldRollRaw;
-        float _yaw;
-        float _pitch;
-        float _roll;
-    
+        Adafruit_BNO055 *_bno;
+        sensor_t _sensor;
+        sensors_event_t _event;
+        uint8_t _system_status = 0;
+        uint8_t _self_test_results = 0;
+        uint8_t _system_error = 0;
+        uint8_t _systemCal = 0;
+        uint8_t _gyroCal = 0;
+        uint8_t _accelCal = 0;
+        uint8_t _magCal = 0;
+
+        double _oldYawRaw = 0;
+        double _oldPitchRaw = 0;
+        double _oldRollRaw = 0;
+        double _yaw = 0;
+        double _pitch = 0;
+        double _roll = 0;
+
+        const unsigned int _BNO055_SAMPLERATE_DELAY_MS = 100;
+
     public:
-        IMU_Wrapper(unsigned int mpuInterruptPin);
+        IMU_Wrapper(unsigned int sensorID = 55, uint8_t address = 0x28);
+
+        void begin(Adafruit_BNO055::adafruit_bno055_opmode_t mode = Adafruit_BNO055::adafruit_bno055_opmode_t::OPERATION_MODE_NDOF);
 
         void update();
 
-        void reset(float yaw = 0, float pitch = 0, float roll = 0);
+        void reset(double yaw = 0, double pitch = 0, double roll = 0);
 
-        float getYaw() const;
+        void displaySensorDetails();
 
-        float getPitch() const;
+        void displaySensorStatus();
 
-        float getRoll() const;
+        void displayCalStatus();
+
+        void displayOrientation();
+
+        double getYaw() const;
+
+        double getPitch() const;
+
+        double getRoll() const;
     
     private:
-        void _overflow(float& oldRaw, float& raw, float& axis);
+        void _overflow(double& oldRaw, double& raw, double& axis);
 };
