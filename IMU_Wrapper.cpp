704bef340fae8d94e78f924f27ae4dbf22129d02
Rohan Dugad <rohand2468@gmail.com>
2021-01-17 01:31:30 -0500

    Added methods from calibration program

--- IMU_Wrapper.cpp
+++ IMU_Wrapper.cpp
@@ -1,13 +1,16 @@
 #include <Arduino.h>
 #include "IMU_Wrapper.h"
 
-IMU_Wrapper::IMU_Wrapper(unsigned int sensorID /*= 55*/, uint8_t address /*= 0x28*/)
+IMU_Wrapper::IMU_Wrapper(unsigned int RST, unsigned int sensorID /*= 55*/, uint8_t address /*= 0x28*/) : _RST(RST)
 {
   _bno = new Adafruit_BNO055(sensorID, address);
 }
 
 void IMU_Wrapper::begin(Adafruit_BNO055::adafruit_bno055_opmode_t mode /*= Adafruit_BNO055::adafruit_bno055_opmode_t::OPERATION_MODE_NDOF*/)
 {
+  pinMode(_RST, OUTPUT);
+  digitalWrite(_RST, HIGH);
+
   if(!_bno->begin(mode))
   {
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
@@ -19,6 +22,11 @@ void IMU_Wrapper::begin(Adafruit_BNO055::adafruit_bno055_opmode_t mode /*= Adafr
   _bno->setExtCrystalUse(true);
 
   update();
+}
+
+void IMU_Wrapper::setExtCrystalUse(bool usextal)
+{
+  _bno->setExtCrystalUse(usextal);
 }
 
 void IMU_Wrapper::update()
@@ -50,6 +58,45 @@ void IMU_Wrapper::reset(double yaw /*=0*/, double pitch /*=0*/, double roll /*=0
   _yaw = yaw;
   _pitch = pitch;
   _roll = roll;
+}
+
+void IMU_Wrapper::resetSensor()
+{
+  digitalWrite(_RST, LOW);
+  delay(1);
+  digitalWrite(_RST, HIGH);
+  delay(800);
+}
+
+/**************************************************************************/
+/*
+    Display the raw calibration offset and radius data
+    */
+/**************************************************************************/
+void IMU_Wrapper::displayOffsets(const adafruit_bno055_offsets_t &calibData)
+{
+  Serial.print("Accelerometer: ");
+  Serial.print(calibData.accel_offset_x); Serial.print(" ");
+  Serial.print(calibData.accel_offset_y); Serial.print(" ");
+  Serial.print(calibData.accel_offset_z); Serial.print(" ");
+
+  Serial.print("\nGyro: ");
+  Serial.print(calibData.gyro_offset_x); Serial.print(" ");
+  Serial.print(calibData.gyro_offset_y); Serial.print(" ");
+  Serial.print(calibData.gyro_offset_z); Serial.print(" ");
+
+  Serial.print("\nMag: ");
+  Serial.print(calibData.mag_offset_x); Serial.print(" ");
+  Serial.print(calibData.mag_offset_y); Serial.print(" ");
+  Serial.print(calibData.mag_offset_z); Serial.print(" ");
+
+  Serial.print("\nAccel Radius: ");
+  Serial.print(calibData.accel_radius);
+
+  Serial.print("\nMag Radius: ");
+  Serial.print(calibData.mag_radius);
+
+  Serial.print("\n");
 }
 
 /**************************************************************************/
@@ -140,6 +187,43 @@ double IMU_Wrapper::getPitch() const
 double IMU_Wrapper::getRoll() const
 {
   return _roll;
+}
+
+/**************************************************************************/
+/*
+    Get the raw calibration offset and radius data [UNOPTIMIZED]
+    */
+/**************************************************************************/
+adafruit_bno055_offsets_t IMU_Wrapper::getOffsets()
+{
+  _systemCal = 0;
+  _gyroCal = 0;
+  _accelCal = 0;
+  _magCal = 0;
+
+  while (!(_systemCal == 3 && _gyroCal == 3 && _accelCal == 3 && _magCal == 3))
+  {
+    /* Poll sensor for new data */
+    update();
+
+    /* Optional: Display orientation data */
+    displayOrientation();
+
+    /* Wait the specified delay before requesting new data */
+    delay(_BNO055_SAMPLERATE_DELAY_MS);
+
+    /* Optional: Display calibration status */
+    displayCalStatus();
+    }
+
+    Serial.println("\nFully calibrated!");
+
+    //Fetch results
+    adafruit_bno055_offsets_t newCalib;
+    _bno->getSensorOffsets(newCalib);
+
+    //Return offsets
+    return newCalib;
 }
 
 void IMU_Wrapper::_overflow(double& oldRaw, double& raw, double& axis)
--- IMU_Wrapper.h
+++ IMU_Wrapper.h
@@ -25,14 +25,24 @@ class IMU_Wrapper
         const unsigned int _BNO055_SAMPLERATE_DELAY_MS = 100;
         unsigned int _lastUpdated_MS;
 
+        const unsigned int _RST;
+
     public:
-        IMU_Wrapper(unsigned int sensorID = 55, uint8_t address = 0x28);
+        IMU_Wrapper(unsigned int RST, unsigned int sensorID = 55, uint8_t address = 0x28);
 
         void begin(Adafruit_BNO055::adafruit_bno055_opmode_t mode = Adafruit_BNO055::adafruit_bno055_opmode_t::OPERATION_MODE_NDOF);
+
+        void setExtCrystalUse(bool usextal);
 
         void update();
 
         void reset(double yaw = 0, double pitch = 0, double roll = 0);
+
+        void resetSensor();
+
+        static void displayOffsets(const adafruit_bno055_offsets_t &calibData);
+
+        void displayOffsets();
 
         void displaySensorDetails();
 
@@ -47,7 +57,9 @@ class IMU_Wrapper
         double getPitch() const;
 
         double getRoll() const;
-    
+
+        adafruit_bno055_offsets_t getOffsets();
+
     private:
         void _overflow(double& oldRaw, double& raw, double& axis);
 };
