6f9c98f939999609db1f2df8064b538455ef513c
Rohan Dugad <rohand2468@gmail.com>
2021-01-27 02:37:38 -0500

    Implemented state machine and packets

--- Serial_Wrapper.cpp
+++ Serial_Wrapper.cpp
@@ -3,6 +3,8 @@
 
 UARTClass Serial_Wrapper::_port = Serial;
 
+size_t Serial_Wrapper::_itemNum = 0;
+
 Serial_Wrapper::Serial_Wrapper(){};
 
 void Serial_Wrapper::begin(uint32_t baudRate, UARTClass& port)
@@ -34,20 +36,80 @@ void Serial_Wrapper::setDefault(const UARTClass& port)
 
 void Serial_Wrapper::send(const uint8_t* buffer, size_t bufferLen, UARTClass& port /*= _port*/)
 {
+    port.write(_DELIMITER_BYTE);
     for (size_t item = 0; item < bufferLen; item++)
     {
-        port.write(buffer[item]);
+        if (buffer[item] == _DELIMITER_BYTE || buffer[item] == _ESCAPE_BYTE)
+        {
+            port.write(_ESCAPE_BYTE);
+            port.write(_escape(buffer[item]));
+        }
+        else
+        {
+            port.write(buffer[item]);
+        }
     }
+    port.write(_DELIMITER_BYTE);
 }
 
 size_t Serial_Wrapper::receive(uint8_t* buffer, size_t bufferLen, UARTClass& port /*= _port*/)
 {
-    size_t itemNum = 0;
     while (port.available())
     {
-        buffer[itemNum] = port.read();
-        itemNum++;
+        if (_receiveSM(buffer, &_itemNum, bufferLen, port.read()))
+        {
+            if (_itemNum == 0)
+            {
+                continue;
+            }
+
+            size_t itemNum = _itemNum;
+            _itemNum = 0;
+            return itemNum;
+        }
     }
 
-    return itemNum;
+    return 0xFFFFFFFF; //return -1
+}
+
+bool Serial_Wrapper::_receiveSM(uint8_t *buffer, size_t *itemNum, size_t bufferLen, uint8_t byte_in)
+{
+    static _State state = _State::INIT;
+    switch (state)
+    {
+        case _State::INIT:
+            if (byte_in == _DELIMITER_BYTE)
+            {
+                state = _State::NORMAL;
+            }
+            return false;
+
+        case _State::NORMAL:
+            if (byte_in == _DELIMITER_BYTE)
+            {
+                return true;
+            }
+            if (byte_in == _ESCAPE_BYTE)
+            {
+                state = _State::ESCAPE;
+                return false;
+            }
+            buffer[(*itemNum)++] = byte_in;
+            return false;
+        
+        case _State::ESCAPE:
+            buffer[(*itemNum)++] = _unescape(byte_in);
+            state = _State::NORMAL;
+            return false;
+    }
+}
+
+uint8_t Serial_Wrapper::_escape(uint8_t raw)
+{
+    return raw ^ _CONVERSION;
+}
+
+uint8_t Serial_Wrapper::_unescape(uint8_t escaped)
+{
+    return escaped ^ _CONVERSION;
 }
--- Serial_Wrapper.h
+++ Serial_Wrapper.h
@@ -4,9 +4,22 @@
 class Serial_Wrapper
 {
     private:
-        static const uint8_t _crc_calculator[0x20];
+        static const uint8_t _CRC_CALCULATOR[0x20];
+
+        static constexpr uint8_t _DELIMITER_BYTE = 0xff;
+        static constexpr uint8_t _ESCAPE_BYTE = 0xfe;
+        static constexpr uint8_t _CONVERSION = 0x80;
 
         static UARTClass _port;
+
+        enum class _State
+        {
+            INIT,
+            NORMAL,
+            ESCAPE
+        };
+
+        static size_t _itemNum;
 
     public:
         static void begin(uint32_t baudRate, UARTClass& port);
@@ -20,7 +33,13 @@ class Serial_Wrapper
     private:
         Serial_Wrapper();
 
-        static uint8_t _doCRC(uint8_t incomingByte);
+        static bool _receiveSM(uint8_t *buffer, size_t *i, size_t bufferLen, uint8_t byte_in);
 
-        static uint8_t _undoCRC(uint8_t incomingByte);
+        static uint8_t _doCRC(uint8_t byte_in);
+
+        static uint8_t _undoCRC(uint8_t byte_in);
+
+        static uint8_t _escape(uint8_t raw);
+
+        static uint8_t _unescape(uint8_t escaped);
 };
