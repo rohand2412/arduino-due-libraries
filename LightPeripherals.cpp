ba129e781b81cd699b5d2762b9e8f18af6bbb2bb
Rohan Dugad <rohand2468@gmail.com>
2021-03-21 02:39:07 -0400

    Added pwm support

--- LightPeripherals.cpp
+++ LightPeripherals.cpp
@@ -64,37 +64,63 @@ void LED::_update()
     }
 }
 
-RGB_LED::RGB_LED(unsigned int rPin, unsigned int bPin, unsigned int gPin)
-    : _red(rPin), _blue(bPin), _green(gPin)
+RGB_LED::RGB_LED(unsigned int rPin, unsigned int gPin, unsigned int bPin)
+    : _red(rPin), _green(gPin), _blue(bPin)
 {}
 
 void RGB_LED::begin()
 {
-    _red.begin();
-    _blue.begin();
-    _green.begin();
+    for (size_t led = 0; led < LED_NUM; led++)
+    {
+        _leds[led].begin();
+    }
 }
 
-void RGB_LED::setState(bool rState, bool bState, bool gState)
+void RGB_LED::setPwm(uint8_t pwm, size_t led)
 {
-    _red.setState(rState);
-    _blue.setState(bState);
-    _green.setState(gState);
+    _leds[led].setPwm(pwm);
+}
+
+void RGB_LED::setPwm(uint8_t rPwm, uint8_t gPwm, uint8_t bPwm)
+{
+    setPwm(rPwm, RED_INDEX);
+    setPwm(gPwm, GREEN_INDEX);
+    setPwm(bPwm, BLUE_INDEX);
+}
+
+uint8_t RGB_LED::getPwm(size_t led) const
+{
+    return _leds[led].getPwm();
+}
+
+void RGB_LED::setState(bool state)
+{
+    for (size_t led = 0; led < LED_NUM; led++)
+    {
+        _leds[led].setState(state);
+    }
 }
 
 void RGB_LED::toggle()
 {
-    _red.toggle();
-    _blue.toggle();
-    _green.toggle();
+    for (size_t led = 0; led < LED_NUM; led++)
+    {
+        _leds[led].toggle();
+    }
 }
 
 void RGB_LED::on()
 {
-    setState(true, true, true);
+    for (size_t led = 0; led < LED_NUM; led++)
+    {
+        _leds[led].on();
+    }
 }
 
 void RGB_LED::off()
 {
-    setState(false, false, false);
+    for (size_t led = 0; led < LED_NUM; led++)
+    {
+        _leds[led].off();
+    }
 }
--- LightPeripherals.h
+++ LightPeripherals.h
@@ -35,17 +35,31 @@ class LED
 
 class RGB_LED
 {
+    public:
+        static const size_t LED_NUM = 3;
+        static const size_t RED_INDEX = 0;
+        static const size_t GREEN_INDEX = 1;
+        static const size_t BLUE_INDEX = 2;
+
     private:
         LED _red;
-        LED _blue;
         LED _green;
-    
+        LED _blue;
+
+        LED _leds[LED_NUM] = {_red, _green, _blue};
+
     public:
-        RGB_LED(unsigned int rPin, unsigned int bPin, unsigned int gPin);
+        RGB_LED(unsigned int rPin, unsigned int gPin, unsigned int bPin);
 
         void begin();
 
-        void setState(bool rState, bool bState, bool gState);
+        void setPwm(uint8_t pwm, size_t led);
+
+        void setPwm(uint8_t rPwm, uint8_t gPwm, uint8_t bPwm);
+
+        uint8_t getPwm(size_t led) const;
+
+        void setState(bool state);
 
         void toggle();
 
